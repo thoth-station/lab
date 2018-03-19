b599fdf2dde741506b8c0cb16c235b300e6b4f0a
Fridolin Pokorny <fridolin@redhat.com>
2018-03-19 13:57:50 +0100

    Add GraphResultQuery object

--- thoth/lab/__init__.py
+++ thoth/lab/__init__.py
@@ -1,3 +1,4 @@
 """Routines for experiments in Thoth not only for Jupyter notebooks."""
 
 from .utils import obtain_location
+from .graph import GraphQueryResult
---
+++ thoth/lab/graph.py
@@ -0,0 +1,48 @@
+"""Various helpers and utils for interaction with the graph database."""
+
+import asyncio
+import typing
+
+import pandas as pd
+from plotly.offline import init_notebook_mode, iplot
+import plotly.graph_objs as go
+
+
+class GraphQueryResult(object):
+    def __init__(self, result):
+        """Initialization.
+
+        :param result: the result to be used as a query result, can be directly coroutine that is fired.
+        """
+        if isinstance(result, typing.Coroutine):
+            loop = asyncio.get_event_loop()
+            result = loop.run_until_complete(result)
+
+        self.result = result
+
+    def _get_items(self):
+        """Get items from the result."""
+        items = list(self.result.items())
+        labels = [item[0] for item in items]
+        values = [item[1] for item in items]
+        return labels, values
+
+    def to_dataframe(self):
+        """Construct a panda's dataframe on results."""
+        return pd.DataFrame(data=self.result)
+
+    def plot_pie(self):
+        """Plot a pie of results into Jupyter notebook."""
+        init_notebook_mode(connected=True)
+
+        labels, values = self._get_items()
+        trace = go.Pie(labels=labels, values=values)
+        return iplot([trace])
+
+    def plot_bar(self):
+        """Plot histogram of results obtained."""
+        init_notebook_mode(connected=True)
+
+        labels, values = self._get_items()
+        trace = go.Bar(x=labels, y=values)
+        return iplot([trace])
