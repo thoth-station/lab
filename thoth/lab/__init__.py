ea7c13dfbe2164e6e27f894c9bf1ddc1b38e55f1
Fridolin Pokorny <fridolin@redhat.com>
2018-03-26 12:59:45 +0200

    Provide version properly

--- thoth/lab/__init__.py
+++ thoth/lab/__init__.py
@@ -1,5 +1,8 @@
 """Routines for experiments in Thoth not only for Jupyter notebooks."""
 
+__name__ = 'thoth-lab'
+__version__ = '0.0.3'
+
 from .utils import obtain_location
 from .graph import GraphQueryResult
 from .utils import packages_info
